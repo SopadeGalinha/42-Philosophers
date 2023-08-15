0e5dec8e8e142325de8a09e8501b60a7bdbdc97e
Hellom-World <hellom-mendes@hotmail.com>
2023-08-15 09:08:32 +0100

    comeca tratamento dos inputs

--- philo/main.c
+++ philo/main.c
@@ -1,4 +1,41 @@
+int	ft_atoi(const char *str)
+{
+	int	c;
+	int	s;
+	int	res;
+
+	c = 0;
+	s = 1;
+	res = 0;
+	while (str[c] == ' ' || str[c] == '\n' || str[c] == '\t'
+		|| str[c] == '\v' || str[c] == '\f' || str[c] == '\r')
+		c++;
+	if (str[c] == '-' || str[c] == '+')
+	{
+		if (str[c] == '-')
+			s = -1;
+		c++;
+	}
+	while (str[c] >= '0' && str[c] <= '9')
+	{
+		res = (res * 10) + (str[c] - 48);
+		c++;
+	}
+	return (res * s);
+}
+
 int main(int argc, char **argv)
 {
-    
+    if (argc < 5 || argc > 6)
+        return (printf("error na quantidade de argumentos"));
+    if (ft_atoi(argv[1]) < 1)
+        return (printf("Quantidade de filosofo invalida"));
+    if (ft_atoi(argv[2]) < 1)
+        return (printf("Tempo de vida invalido"));
+    if (ft_atoi(argv[3]) < 1)
+        return (printf("Tempo para comer invalido"));
+    if (ft_atoi(argv[4]) < 1)
+        return (printf("Tempo para dormir invalido"));
+    if (argc != 6)
+    return (0);
 }
